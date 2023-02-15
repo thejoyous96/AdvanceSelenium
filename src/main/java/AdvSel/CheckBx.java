8c77cd62c46ea7ab9960e108e52334353e06ef5e
Sujoy Sarkar <sujoysarkar96@gmail.com>
2023-02-15 18:17:06 -0500

    First Commit of Advance Selenium

--- src/main/java/AdvSel/CheckBx.java
+++ src/main/java/AdvSel/CheckBx.java
@@ -1,9 +1,59 @@
 package AdvSel;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CheckBx {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://the-internet.herokuapp.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		
+		driver.findElement(By.xpath("//a[text()='Checkboxes']")).click();
+		
+		WebElement box1 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
+		
+		WebElement box2 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]"));
+				
+		
+		Thread.sleep(3000);
+
+	
+		if (box1.isSelected()) {
+			
+			System.out.println("CheckBox 1 was already selected");
+
+		}
+		
+		else 
+		
+		{
+			box1.click();
+		}
+		
+		
+		Thread.sleep(3000);
+		
+		
+		driver.close();
 		
 	}
-	
+
 }
+
--- src/main/java/AdvSel/DragDrop.java
+++ src/main/java/AdvSel/DragDrop.java
@@ -1,8 +1,45 @@
 package AdvSel;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DragDrop {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		WebElement f = driver.findElement(By.xpath("(//div[text()='Washington'])[2]"));
+		
+		WebElement t = driver.findElement(By.xpath("//div[text()='United States']"));
+		
+		
+		Actions act = new Actions(driver);
+		
+		act.dragAndDrop(f, t).perform();
+		
+		
+		Thread.sleep(4000);
+		
+		
+		driver.close();
 		
 	}
 }
